8ba8515fd1927358ba74954d0c53ec7e6b851b8c
Ana K <@anakodeq>
2023-07-26 23:14:37 +0300

    Task00 updated

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.Write($"The square of {number} is: ");
+Console.WriteLine(square);
