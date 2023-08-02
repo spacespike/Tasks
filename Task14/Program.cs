840d446ef65f04e34683a9ccf3d8346ce9423b23
Ana K <@anakodeq>
2023-08-02 18:08:58 +0300

    Task14  added

---
+++ Task14/Program.cs
@@ -0,0 +1,29 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Write the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+bool result = IsDivisible(number);
+Console.WriteLine($"The result is {result}");
+
+bool IsDivisible(int num)
+{
+    int remainder1 = num % 7;
+    int remainder2 = num % 23;
+    if (remainder1 == 0 && remainder2 == 0)
+    {
+        Console.WriteLine($"{num} is divisible by 7 and 23");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine($"{num} is not divisible by 7 or 23");
+        return false;
+    }
+}
