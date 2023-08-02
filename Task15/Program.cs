e3e91814bc8cc064798c8458191bc5cb75fbb644
Ana K <@anakodeq>
2023-08-02 20:12:39 +0300

    Task15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// get the number of a weekday 
+// retuns if it is a weekend or no
+
+bool IsWeekend (int num){
+    return (num == 6 || num == 7);
+}
+
+
+Console.WriteLine("Write the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsWeekend(number);
+Console.WriteLine(result ? "Yes" : "No");
