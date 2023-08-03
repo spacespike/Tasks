8416ab96207d3e27c0dbd9cdad444568259d400c
Ana K <@anakodeq>
2023-08-03 19:37:35 +0300

    Task19 modified

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,26 +4,32 @@
 // 12821 -> да
 // 23432 -> да
 
-bool IsFiveDigit (int num) {
+bool IsFiveDigit(int num)
+{
     return num > 9999 && num < 100000 ? true : false;
 }
 
 
-void Palindrome (int num) {
-    if (IsFiveDigit(num) == true) {
+void Palindrome(int num)
+{
+    if (IsFiveDigit(num) == true)
+    {
         int first = num / 10000;
         int second = num / 1000 % 10;
         int fourth = num / 10 % 10;
         int fifth = num % 10;
 
-        if (first == fifth && second == fourth) {
+        if (first == fifth && second == fourth)
+        {
             Console.WriteLine($"{num} is a palindrome!");
-        } else Console.WriteLine($"{num} is not a palindrome!");
-        // string result = first == fifth && second == fourth   //Pathetic efforts to tame ternary operator in various scenarious
+        }
+        else Console.WriteLine($"{num} is not a palindrome!");
+        // string result = first == fifth && second == fourth   //Pathetic efforts to use ternary operator in various scenarious
         //                 ? Console.WriteLine($"{num} is a palindrome!") 
         //                 : Console.WriteLine($"{num}is not a palindrome!");
 
-    } else Console.WriteLine($"{num} is not five  digit number");
+    }
+    else Console.WriteLine($"{num} is not five  digit number");
 }
 
 
