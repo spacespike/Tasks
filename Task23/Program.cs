a7589b3cf157ed7ee255da24614b31a78bf5b2b9
Ana K <@anakodeq>
2023-08-03 20:00:23 +0300

    Task23 added

---
+++ Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23:
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void CubeTable(int num)
+{
+    if (IsHigherThan1(num) == true)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            int cube = i * i * i;
+            Console.WriteLine($"{i,3} {cube,7}");
+        }
+    }
+    else Console.WriteLine("Incorrect data");
+}
+
+bool IsHigherThan1(int num)
+{
+    return num >= 1 ? true : false;
+}
+
+Console.Write("Define the N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+CubeTable(number);
