dee505c2d2657680910f98566939703201750b94
Ana K <@anakodeq>
2023-08-03 19:55:52 +0300

    Task21 added

---
+++ Task21/Program.cs
@@ -0,0 +1,33 @@
+﻿// 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double DistanseTwoPoints(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    int cat1 = x1 - x2;
+    int cat2 = y1 - y2;
+    int cat3 = z1 - z2;
+    double result = Math.Sqrt(cat1 * cat1 + cat2 * cat2 + cat3 * cat3);
+    return result;
+}
+
+
+Console.WriteLine("First point coordinats");
+Console.Write("X: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Second point coordinats");
+Console.Write("X: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Round(DistanseTwoPoints(x1Coordinate, y1Coordinate, z1Coordinate, x2Coordinate, y2Coordinate, z2Coordinate), 2, MidpointRounding.ToZero);
+Console.WriteLine(result);
