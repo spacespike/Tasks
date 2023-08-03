f6c9a0462456360e1f474fa06067e26d77549271
Ana K <@anakodeq>
2023-08-03 18:56:06 +0300

    Task22 modified

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -10,7 +10,7 @@ void SquareTable(int num)
         for (int i = 1; i <= num; i++)
         {
             int square = i * i;
-            Console.WriteLine($"{i}     {square}");
+            Console.WriteLine($"{i,3} {square,5}");
         }
     }
     else Console.WriteLine("Incorrect data");
