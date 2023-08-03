8506e77a21cb51bb7012493de79d3ae33a789c63
Ana K <@anakodeq>
2023-08-03 15:30:13 +0300

    Task18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// 18. Напишите программу, которая номер четверти плоскости, принимает на вход
+//и выдаёт координаты точки (X и Y)
+// в которой может находится эта
+// точка.
+
+string Quarter(int quarter)
+{
+    if (quarter == 1) return "x > 0 && y > 0";
+    if (quarter == 2) return "x < 0 && y > 0";
+    if (quarter == 3) return "x < 0 && y < 0";
+    if (quarter == 4) return "x > 0 && y < 0";
+    return "imposssible quarter";
+}
+
+
+Console.Write("Write the quarter of the dot: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = Quarter(number);
+// string result = quarter > 0
+//                 ? $"Coordinats belong to {quarter} quarter"
+//                 : "Incorrect coordinats";
+
+Console.WriteLine(result);
