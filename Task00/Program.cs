db973f32c9290430c39825c495ef16fe1353f7b3
Ana K <@anakodeq>
2023-08-13 12:35:43 +0300

    Task 30 modified

--- Task00/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.Write ("Enter a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.Write($"The square of {number} is: ");
-Console.WriteLine(square);
