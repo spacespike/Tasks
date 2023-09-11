f2a4b9575b8f1ea35b39b9cc9ec8943e21c66ac3
Ana K <@anakodeq>
2023-09-11 09:32:53 +0300

    Task 58 added

---
+++ Task58/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++) // matrix.GetLength(0)
+    {
+        for (int j = 0; j < colums; j++) // matrix.GetLength(1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine("  |");
+    }
+
+}
+
+int[,] MultiplyMatrices(int[,] firstMatrix, int[,] secondMatrix)
+{
+    // For instances with not square matrices
+    int newRow, newColumn;
+    if (firstMatrix.GetLength(0) > secondMatrix.GetLength(0))
+    {
+        newRow = secondMatrix.GetLength(0);
+    }
+    else
+    {
+        newRow = firstMatrix.GetLength(0);
+    }
+
+    if (firstMatrix.GetLength(1) > secondMatrix.GetLength(1))
+    {
+        newColumn = secondMatrix.GetLength(1);
+    }
+    else
+    {
+        newColumn = firstMatrix.GetLength(1);
+    }
+
+    int[,] newMartix = new int[newRow, newColumn];
+
+    for (int i = 0; i < newRow; i++)
+    {
+        for (int j = 0; j < newColumn; j++)
+        {
+            for (int k = 0; k < newRow; k++)
+            {
+                newMartix[i, j] += firstMatrix[i, k] * secondMatrix[k, j];
+            }
+        }
+    }
+    return newMartix;
+}
+
+Console.Write("Enter amount of rows for the matrices: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter amount of colums for the matrices: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter max value for the matrices: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter min value for the matrices: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstMatrix = CreateMatrixRndInt(rows, colums, min, max);
+Console.WriteLine("First created matrix: ");
+PrintArray(firstMatrix);
+
+int[,] secondMatrix = CreateMatrixRndInt(rows, colums, min, max);
+Console.WriteLine("Second created matrix: ");
+PrintArray(secondMatrix);
+
+Console.WriteLine("Multiplied matrices: ");
+int[,] newMartix = MultiplyMatrices(firstMatrix, secondMatrix);
+PrintArray(newMartix);
