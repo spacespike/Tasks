a0e85280a2fa9fe01973833ac5fb95384f047d86
Ana K <@anakodeq>
2023-09-14 10:03:57 +0300

    Task 66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int SumFromMToN(int firstNumber, int secondNumber)
+{
+    int result = firstNumber;
+    if (firstNumber == secondNumber)    //not to end up adding first number twice
+        return 0;
+    else if (secondNumber < firstNumber)
+        return 0;
+    else
+    {
+        firstNumber++;
+        result = firstNumber + SumFromMToN(firstNumber, secondNumber);
+        return result;
+    }
+}
+
+
+Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(m + SumFromMToN(m, n));   //added first number here
